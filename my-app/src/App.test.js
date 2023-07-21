5abe6a069c17650af0ab896a1c480dc5e2eb4c8c
Calvin Chan <calvinchan@Calvins-MacBook-Pro.local>
2023-07-21 09:25:10 +0800

    Added new test conditions

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,6 +2,12 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'; 
 import App from './App';
 
+test('checkbox is initially unchecked', () => {
+  render(<App />);
+  const checkboxInput = screen.getByTestId("checkbox");
+  expect(checkboxInput).not.toBeChecked();
+});
+
 test('check checkbox functionality', () => {
   render(<App />);
   userEvent.click(screen.getByTestId("checkbox"));
